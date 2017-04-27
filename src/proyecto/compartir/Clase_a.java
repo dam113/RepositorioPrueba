59340b01420385ce04aecd9773c531ee1aaaf0fd
DAM113 <DAM113@example.com>
2017-04-27 12:15:55 +0200

    version con clase a

---
+++ src/proyecto/compartir/Clase_a.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.compartir;
+
+/**
+ *
+ * @author dam113
+ */
+public class Clase_a {
+  
+}
